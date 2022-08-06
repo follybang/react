4a48cbf0ac63e59cc9fd99344ac153d1708f75b0
Bohdan <drabynko.b@gmail.com>
2022-08-06 18:07:33 +0300

    done

---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "review_build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+      { test: /.js$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
