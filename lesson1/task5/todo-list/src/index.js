be93d600145ca0a42e23ef8da9192e83f5f85057
Bohdan <drabynko.b@gmail.com>
2022-08-08 20:53:17 +0300

    done

---
+++ lesson1/task5/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const rootElement = document.querySelector("#root");
+
+const elem = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(elem, rootElement);
