2d4d7ef9a3039954ad1fbaa3d5fc0678b46c7aa2
Bohdan <drabynko.b@gmail.com>
2022-08-21 22:56:29 +0300

    done

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import moment from "moment";
 
 const Transaction = ({ from, to, amount, rate, time }) => (
-  <li class="transaction">
-    <span class="transaction__date">{moment(time).format("D MMM")}</span>
-    <span class="transaction__time">{moment(time).format("H:mm")}</span>
-    <span class="transaction__assets">
+  <li className="transaction">
+    <span className="transaction__date">{moment(time).format("D MMM")}</span>
+    <span className="transaction__time">{moment(time).format("H:mm")}</span>
+    <span className="transaction__assets">
       {from} → {to}
     </span>
-    <span class="transaction__rate">{rate}</span>
-    <span class="transaction__amount">{new Intl.NumberFormat("en-GB").format(amount)}</span>
+    <span className="transaction__rate">{rate}</span>
+    <span className="transaction__amount">{new Intl.NumberFormat("en-GB").format(amount)}</span>
   </li>
 );
 
