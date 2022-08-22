d17fdc8cb2faa467cb54f5021d6a94fca6b36bd2
Bohdan <drabynko.b@gmail.com>
2022-08-22 19:04:51 +0300

    done

---
+++ lesson8/task2/src/Clock.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Clock extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+}
+
+export default Clock;
---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Dimensions extends Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+
+    const { innerWidth, innerHeight } = window;
+    this.state = {
+      width: innerWidth,
+      height: innerHeight,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+
+  render() {
+    return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
+  }
+}
+
+export default Dimensions;
