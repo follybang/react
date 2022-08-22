88ee7b5bf0dbc3c21ec2aef037998fe12b090632
Bohdan <drabynko.b@gmail.com>
2022-08-22 20:59:37 +0300

    done

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 
 class ConnectionStatus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: "online",
-    };
-  }
+  state = {
+    status: "online",
+  };
 
   componentDidMount() {
     window.addEventListener("online", this.onOnline);
