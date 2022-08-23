33940db02d58767405ea97acb40817ed2545bcf3
Bohdan <drabynko.b@gmail.com>
2022-08-23 23:02:41 +0300

    done

--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -7,7 +7,7 @@ class UserForm extends Component {
     this.state = {
       name: "",
       student: "",
-      occupation: "london",
+      occupation: "",
       about: "",
     };
   }
@@ -22,14 +22,14 @@ class UserForm extends Component {
     });
   };
 
-  handleSubmit = event => {
+  onSubmit = event => {
     event.preventDefault();
     this.props.createUser(this.state);
   };
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
+      <form className="login-form" onSubmit={this.onSubmit}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
