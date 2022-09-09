bd37ada5c9c195bfd83f55e52ba2cabe1aa5c4c8
Bohdan <drabynko.b@gmail.com>
2022-09-09 22:12:08 +0300

    done

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Game from "./Game.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
